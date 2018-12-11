5b711d13f9ce1a711a8ac837d4727601c77ef2da
오재민 <gerrard13@naver.com>
2018-12-11 20:35:37 +0900

    #43 define new function

--- utils.h
+++ utils.h
@@ -10,10 +10,12 @@ namespace IPCVL {
 	namespace UTIL {
 		enum CONNECTIVITIES {
 			NAIVE_FOURWAY = 0,
-			NAIVE_EIGHT_WAY,
-			EFFICIENT_FOURWAY
+			NAIVE_EIGHT_WAY = 1,
+			EFFICIENT_FOURWAY = 2
 		};
 		int quantize(int a);
+		double make_u(cv::InputArray src, double *histogram);
+		void makeBtoL(cv::InputArray src, cv::OutputArray dst);
 		double h_r(double model_hist[][64], double input_hist[][64], int j, int i);
 		void GetHistogramImage(int* histogram, cv::OutputArray dst, int hist_w, int hist_h);
 		void calcNormedHist(cv::InputArray src, double* histogram);
