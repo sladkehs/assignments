1e508f51355fb33c3d10285f33ede1f6a1ca2e18
오재민 <gerrard13@naver.com>
2018-12-11 20:34:13 +0900

    #43 make new function thresh_binary, thresh_otsu, flood_fill.

--- imgproc.cpp
+++ imgproc.cpp
@@ -1,8 +1,10 @@
 #include "imgproc.h"
+#include <iostream>
+#include <queue>
 
 namespace IPCVL {
 	namespace IMG_PROC {
-		void calcHist(cv::InputArray src, int* histogram) {
+		void calcHist(cv::InputArray src, double *histogram) {
 			cv::Mat inputMat = src.getMat();
 
 			for (int y = 0; y < inputMat.rows; y++) {
@@ -10,11 +12,13 @@ namespace IPCVL {
 					// Todo : histogram을 쌓습니다. 
 					/** your code here! **/
 					histogram[inputMat.at<uchar>(y, x)]++;
+					
 					// hint 1 : for loop 를 이용해서 cv::Mat 순회 시 (1채널의 경우) 
 					// inputMat.at<uchar>(y, x)와 같이 데이터에 접근할 수 있습니다. 
 				}
 			}
 		}
+
 		void backprojectHistogram(cv::InputArray src_hsv, cv::InputArray face_hsv, cv::OutputArray dst) {
 			cv::Mat srcMat = src_hsv.getMat();
 			cv::Mat faceMat = face_hsv.getMat();
@@ -77,6 +81,237 @@ namespace IPCVL {
 				}
 			}
 		}
+
+		void thresh_binary(cv::InputArray src, cv::OutputArray dst, const int& threshold)
+		{
+			cv::Mat srcMat = src.getMat();
+
+			dst.create(srcMat.size(), srcMat.type());
+			cv::Mat output = dst.getMat();
+
+			for (int y = 0; y < srcMat.rows; y++)
+			{
+				for (int x = 0; x < srcMat.cols; x++)
+				{
+					if (srcMat.at<uchar>(y, x) > threshold)
+					{
+						output.at<uchar>(y, x) = 255;
+					}
+					else
+					{
+						output.at<uchar>(y, x) = 0;
+					}
+
+				}
+			}
+		}
+
+		void thresh_otsu(cv::InputArray src, cv::OutputArray dst)
+		{
+			dst.create(src.size(), CV_8UC1);
+			cv::Mat srcMat = src.getMat();
+			cv::Mat output = dst.getMat();
+
+			double histogram[256] = { 0, };
+
+			IPCVL::UTIL::calcNormedHist(srcMat, histogram);
+			double general_u = IPCVL::UTIL::make_u(srcMat, histogram);
+
+			std::cout << general_u << std::endl;
+			double w[256];
+			double u0[256];
+			double u1[256];
+			double v[256];
+			int T = 0;
+		
+			w[0] = histogram[0];
+			u0[0] = 0.0;
+			v[0] = 0.0;
+			double max = 0.;
+			for (int t = 1; t < 256; t++)
+			{
+				w[t] = w[t - 1] + histogram[t];
+
+				if (w[t] == 0.0 || (1 - w[t]) == 0.0)
+					continue;
+
+				u0[t] = (w[t - 1] * u0[t - 1] + t * histogram[t]) / w[t];
+				u1[t] = (general_u - w[t] * u0[t]) / (1 - w[t]);
+				v[t] = w[t] * (1 - w[t])*(u0[t] - u1[t])*(u0[t] - u1[t]);
+
+				std::cout << v[t] << std::endl;
+				if (v[t] > max)
+				{
+					max = v[t];
+
+					T = t;
+				}
+
+			}
+
+			for (int y = 0; y < srcMat.rows; y++)
+			{
+				for (int x = 0; x < srcMat.cols; x++)
+				{
+					if (srcMat.at<uchar>(y, x) > T)
+					{
+						output.at<uchar>(y, x) = 255;
+					}
+					else
+					{
+						output.at<uchar>(y, x) = 0;
+					}
+
+				}
+			}
+		}
+
+		void flood_fill4(cv::Mat& l, const int& j, const int& i, const int& label)
+		{
+			if (l.at<int>(j, i) == -1)
+			{
+				l.at<int>(j, i) = label;
+				flood_fill4(l, j, i + 1, label);
+				flood_fill4(l, j - 1, i, label);
+				flood_fill4(l, j, i - 1, label);
+				flood_fill4(l, j + 1, i, label);
+			}
+		}
+
+		void flood_fill8(cv::Mat& l, const int& j, const int& i, const int& label)
+		{
+			if (l.at<int>(j, i) == -1)
+			{
+				l.at<int>(j, i) = label;
+				flood_fill4(l, j, i + 1, label);
+				flood_fill4(l, j - 1, i + 1, label);
+				flood_fill4(l, j - 1, i, label);
+				flood_fill4(l, j - 1, i - 1, label);
+				flood_fill4(l, j, i - 1, label);
+				flood_fill4(l, j + 1, i - 1, label);
+				flood_fill4(l, j + 1, i, label);
+				flood_fill4(l, j + 1, i + 1, label);
+			}
+		}
+
+		void efficient_flood_fill4(cv::Mat& l, const int& j, const int& i, const int& label)
+		{
+			
+
+			std::vector<int> jwapyo;
+			std::queue<std::vector<int> > Q;
+			
+			jwapyo.push_back(j);
+
+			jwapyo.push_back(i);
+
+			Q.push(jwapyo);
+
+			while (!Q.empty())
+			{
+				std::vector<int> tmp;
+
+				tmp = Q.front();
+				Q.pop();
+
+				if (l.at<int>(tmp.front(), tmp.back()) = -1)
+				{
+					int left, right;
+					left = tmp.back();
+					right = tmp.back();
+
+					while (l.at<int>(tmp.front(), left - 1) == -1) left--;
+					while (l.at<int>(tmp.front(), right + 1) == -1) right++;
+
+					for (int c = left; c <= right; c++)
+					{
+						l.at<int>(tmp.front(), c) = label;
+						if ((l.at<int>(tmp.front() - 1, c) == -1) && (c == left || l.at<int>(tmp.front() - 1, c - 1) != -1))
+						{
+							std::vector<int> nextjwapyo;
+							nextjwapyo.push_back(tmp.front() - 1);
+							nextjwapyo.push_back(c);
+							Q.push(nextjwapyo);
+						}
+
+						if ((l.at<int>(tmp.front() + 1, c) == -1) && (c == left || l.at<int>(tmp.front() + 1, c - 1) != -1))
+						{
+							std::vector<int> nextjwapyo;
+							nextjwapyo.push_back(tmp.front() + 1);
+							nextjwapyo.push_back(c);
+							Q.push(nextjwapyo);
+						}
+					}
+				}
+			}
+
+		}
+
+		void flood_fill(cv::InputArray src, cv::OutputArray dst, const UTIL::CONNECTIVITIES& direction)
+		{
+			dst.create(src.size(), CV_32SC1);
+			cv::Mat srcMat = src.getMat();
+			cv::Mat l = dst.getMat();
+
+			IPCVL::UTIL::makeBtoL(srcMat, l);
+
+			/*for (int j = 0; j < l.rows; j++)
+			{
+				for (int i = 0; i < l.cols; i++)
+				{
+					std::cout << l.at<int>(j, i);
+				}
+				std::cout << std::endl;
+			}*/
+
+			if (direction == 0)
+			{
+				int label = 1;
+				for (int j = 1; j < l.rows - 1; j++)
+				{
+					for (int i = 1; i < l.cols - 1; i++)
+					{
+						if (l.at<int>(j, i) == -1)
+						{
+							flood_fill4(l, j, i, label);
+							label++;
+						}
+					}
+				}
+			}
+			else if (direction == 1)
+			{
+				int label = 1;
+				for (int j = 1; j < l.rows - 1; j++)
+				{
+					for (int i = 1; i < l.cols - 1; i++)
+					{
+						if (l.at<int>(j, i) == -1)
+						{
+							flood_fill8(l, j, i, label);
+							label++;
+						}
+					}
+				}
+			}
+			else if (direction == 2)
+			{
+				int label = 1;
+				for (int j = 1; j < l.rows - 1; j++)
+				{
+					for (int i = 1; i < l.cols - 1; i++)
+					{
+						if (l.at<int>(j, i) == -1)
+						{
+							efficient_flood_fill4(l, j, i, label);
+							label++;
+						}
+					}
+				}
+			}
+
+		}
+
 	}  // namespace IMG_PROC
 }
 
